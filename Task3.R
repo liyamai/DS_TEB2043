0100869e89f4c0f6ae6b7c0fae54967b5aadde90
Aliya Maisarah <aliya_24006910@utp.edu.my>
2026-01-30 11:36:22 +0800

    Add program to calculate the area of a circle

---
+++ Task3.R
@@ -0,0 +1,11 @@
+# Program to calculate the area of a circle
+
+# Input for radius
+radius <- as.numeric(readline(prompt = "Enter the radius of the circle: "))
+
+# Calculate Area
+area <- pi * radius^2
+
+# Display result
+cat("The area of the circle is:", area)
+
